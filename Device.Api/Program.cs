e4dc4aa3a1e3d949d77c2f1531062fe9810524cc
Pedro Pagel <pedro.pagel@postnord.com>
2024-03-06 09:50:07 +0000

    #device-api: saving partial

--- Device.Api/Program.cs
+++ Device.Api/Program.cs
@@ -1,11 +1,20 @@
 using Device.Domain.Middlewares;
 using Device.Infrastructure.Configurations;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Device Api", Version = "v1" });
+
+    var xmlFile = $"{typeof(Program).Assembly.GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.ResolveDependencies();
 
@@ -15,7 +24,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Device Api");
+    });
 }
 
 app.UseMiddleware<ExceptionHandlerMiddleware>();
